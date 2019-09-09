c6c9f9d022dd9ac5c25d2d852ebeb017b7bb3691
zhujun.qiu <zhujun.qiu@taotu-partner.com>
2019-09-09 18:03:33 +0800

    image

---
+++ TexturedQuad/TexturedQuad.js
@@ -0,0 +1,121 @@
+var VSHADER_SOURCE = 
+    'attribute vec4 a_Position;\n'+
+    'attribute vec2 a_TexCoord;\n'+
+    'varying vec2 v_TexCoord;\n'+
+    'void main(){\n'+
+    '   gl_Position = a_Position;\n' +
+    '   v_TexCoord = a_TexCoord;\n'+
+    '}\n';
+
+var FSHADER_SOURCE = 
+    'precision mediump float;\n' +
+    'uniform sampler2D u_Sampler;\n'+
+    'varying vec2 v_TexCoord;\n'+
+    'void main() {\n' +
+    '   gl_FragColor = texture2D(u_Sampler, v_TexCoord)\n' +
+    '}\n';
+
+var Tx = 0.5, Ty = 0.5, Tz = 0.0;
+
+var ANGLE = 180.0;
+
+function main()
+{
+    var canvas = document.getElementById('webgl');
+    if(!canvas){
+        console.log('Failed to retrieve the <canvas> element');
+        return;
+    }
+
+    var gl = getWebGLContext(canvas);
+    if(!gl){
+        console.log('Failed to get the rendering context for WebGL');
+        return;
+    }
+
+    if(!initShaders(gl,VSHADER_SOURCE, FSHADER_SOURCE)) {
+        console.log('Failed to initialize shaders.');
+        return;
+    }
+    
+    var n = initVertexBuffers(gl);
+    if(n < 0) {
+        console.log('Failed to set the positions of the vertices');
+        return;
+    }
+
+    gl.clearColor(0.5, 0.5, 0.5, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    if(!initTextures(gl, n)){
+        console.log('failed');
+    }
+    // gl.drawArrays(gl.POINTS, 0, n);
+    // gl.drawArrays(gl.LINE_LOOP, 0, n);
+    // gl.drawArrays(gl.TRIANGLES, 0, n);
+    // gl.drawArrays(gl.TRIANGLE_STRIP, 0, n);
+    // gl.drawArrays(gl.TRIANGLE_FAN, 0, n);
+}
+
+function initVertexBuffers(gl) {
+    var verticesTexCoords = new Float32Array([
+        -0.5,0.5, 0.0, 1.0, 
+        -0.5,-0.5, 0.0, 0.0, 
+        0.5,0.5, 1.0, 1.0,
+        0.5,-0.5, 1.0, 0.0
+    ]);
+    var n = 4;
+
+    var vertexTexCoordBuffer = gl.createBuffer();
+    if(!vertexTexCoordBuffer) {
+        console.log('Failed to create the buffer object ');
+        return -1;
+    }
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexTexCoordBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, verticesTexCoords, gl.STATIC_DRAW);
+
+    var FSIZE = verticesTexCoords.BYTES_PER_ELEMENT;
+
+    var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+    if (a_Position < 0) {
+        console.log('Failed to get the storage location of a_Position');
+        return;
+    }
+    gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, FSIZE * 4, 0);
+    gl.enableVertexAttribArray(a_Position);
+
+    var a_TexCoord = gl.getAttribLocation(gl.program, 'a_TexCoord');
+    gl.vertexAttribPointer(a_TexCoord, 2, gl.FLOAT, false, FSIZE * 4, FSIZE * 2);
+    gl.enableVertexAttribArray(a_TexCoord);
+
+    return n;
+}
+
+function initTextures(gl, n){
+    var texture = gl.createTexture();
+    var u_Sampler = gl.getUniformLocation(gl.program, 'u_Sampler');
+
+    var image = new Image();
+
+    image.onload = function() {
+        loadTexture(gl, n, texture, u_Sampler, image);
+    }
+
+    image.src = '../resources/sky.jpg';
+
+    return true;
+}
+
+function loadTexture(gl, n, texture, u_Sampler, image){
+    gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL,1);
+    gl.activeTexture(gl.TEXTURE0);
+    gl.bindTexture(gl.TEXTURE_2D,texture);
+
+    gl.texParameteri(gl.TEXTURE_2D,gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, gl.RGB, gl.UNSIGNED_BYTE, image);
+
+    gl.uniformli(u_Sampler, 0);
+
+    gl.drawArrays(gl.TRIANGLE_STRIP, 0, n);
+}
